7a60683af5a1731f736390cd21a20bcb6a66d72e
asksbj <dongsj108@hotmail.com>
2026-02-26 11:04:26 +0800

    Add task schedular runable file

---
+++ task_scheduler.py
@@ -0,0 +1,49 @@
+import argparse
+import logging
+
+from jpost.etl.scheduler import JPostTaskScheduler
+
+
+SCHEDULERS = {
+    "jpost": JPostTaskScheduler,
+    # Add more schedulers here in the future, e.g.:
+    # "other": OtherTaskScheduler,
+}
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="TaskScheduler runner entry point."
+    )
+    parser.add_argument(
+        "-s",
+        "--scheduler",
+        choices=SCHEDULERS.keys(),
+        default="jpost",
+        help="Scheduler to run (default: jpost).",
+    )
+    parser.add_argument(
+        "-t",
+        "--threads",
+        type=int,
+        default=5,
+        help="Number of worker threads (default: 5).",
+    )
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
+
+    scheduler_cls = SCHEDULERS[args.scheduler]
+    logging.info(
+        "Starting scheduler %s with %d threads",
+        scheduler_cls.__name__,
+        args.threads,
+    )
+    scheduler_cls.start(args.threads)
+
+
+if __name__ == "__main__":
+    main()
+
