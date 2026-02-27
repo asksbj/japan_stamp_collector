ff7c767bf85b2d92cc400201b78f047d743f1451
asksbj <dongsj108@hotmail.com>
2026-02-27 11:14:02 +0800

    Add manhole card migrator (#14)

--- manhole_card/etl/datatype.py
+++ manhole_card/etl/datatype.py
@@ -1,3 +1,4 @@
 class TaskType:
 
     INGESTOR_MANHOLE_CARD = "ingestor_manhole_card"
+    MIGRATOR_MANHOLE_CARD = "migrator_manhole_card"
--- manhole_card/model.py
+++ manhole_card/model.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from core.database import db_manager
 from models.administration import Facility
@@ -7,14 +7,16 @@ from models.base import BaseModel
 
 class ManholeCard(BaseModel):
     _table_name = "manhole_card"
-    _columns = ["name", "series", "release_date", "image_url"]
+    _columns = ["name", "series", "release_date", "location_info", "distribution_time", "image_url"]
     _db_manager = db_manager
 
     def __init__(self, **kwargs) -> None:
         self.id = kwargs.get("id")
         self.name = kwargs.get("name")
-        self.addseriesress = kwargs.get("series")
+        self.series = kwargs.get("series")
         self.release_date = kwargs.get("release_date")
+        self.location_info = kwargs.get("location_info")
+        self.distribution_time = kwargs.get("distribution_time")
         self.image_url = kwargs.get("image_url")
 
     def location_detail(self) -> List[dict]:
@@ -24,6 +26,19 @@ class ManholeCard(BaseModel):
         for facility in facilities:
             result.append(facility.to_dict())
         return result
+
+    @classmethod
+    def get_by_name_and_series(cls, name: str, series: str) -> "ManholeCard":
+        if not name or not series:
+            return None
+
+        query = (
+            f"SELECT * FROM {cls.get_table_name()} "
+            "WHERE name = %s AND series = %s"
+        )
+        params = (name, series)
+
+        return cls.get_db_results(query, params, fetch_one=True)
 
 
 class ManholeCardFacility(BaseModel):
@@ -36,6 +51,24 @@ class ManholeCardFacility(BaseModel):
         self.manhole_card_id = kwargs.get("manhole_card_id")
         self.facility_id = kwargs.get("facility_id")
 
+    @classmethod
+    def get_by_fuzzy_id(cls, manhole_card_id: Optional[int] = None, facility_id: Optional[int] = None) -> List["ManholeCardFacility"]:
+        if not manhole_card_id and not facility_id:
+            return None
+
+        conditions = []
+        params = []
+        if manhole_card_id is not None:
+            conditions.append("AND manhole_card_id = %s")
+            params.append(manhole_card_id)
+
+        if facility_id is not None:
+            conditions.append("AND facility_id = %s")
+            params.append(facility_id)
+
+        where_clause = " ".join(conditions)
+        query = f"SELECT * FROM {cls.get_table_name()} WHERE 1=1 {where_clause}"
+        return cls.get_db_results(query, tuple(params))
 
     @classmethod
     def get_facilities(cls, manhole_card_id: int) -> List["Facility"]:
